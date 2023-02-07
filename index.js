86ac030fe5b42923deff01f7c9299d112f21565a
aryanGupta-09 <aryangupta0329@gmail.com>
2023-02-07 23:44:42 +0530

    add layout and use partials

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 666;
+const expressLayouts = require("express-ejs-layouts");
 
 app.use(express.static("./assets"));
+
+app.use(expressLayouts);
+// extract styles and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // set up the view engine
 app.set("view engine", "ejs");
