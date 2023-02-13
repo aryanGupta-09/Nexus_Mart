449cbd26b0227b891f7b936829f7ed13d85a958c
Divyansh21321 <divyansh21321@iiitd.ac.in>
2023-02-14 03:21:16 +0530

    merge branch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 666;
 const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/sequelize");
 
 app.use(express.static("./assets"));
 
